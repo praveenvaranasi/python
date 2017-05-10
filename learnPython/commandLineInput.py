cc01087ed8014154e896ae065ce2b25cb34dbd5b
Praveen <praveenb.varanasi@outlook.com>
2017-05-10 13:41:24 +0530

    Inserted Spaces around operators

--- learnPython/commandLineInput.py
+++ learnPython/commandLineInput.py
@@ -1,6 +1,6 @@
 # Program that accepts the input from user and displays it on the Console
 
-String=input("Please enter the String:\n")
+String = input("Please enter the String:\n")
 print(String)
 integer=int(input("Enter some integer"))
 print(integer)
