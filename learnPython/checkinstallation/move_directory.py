0b75e48daeb0a89e389dad8c03fe1c76a9f8f0d8
Praveen <praveenb.varanasi@outlook.com>
2017-05-11 21:34:38 +0530

    Added check_installation_directory module to the checkinstallation package with the class Structure

---
+++ learnPython/checkinstallation/move_directory.py
@@ -0,0 +1,8 @@
+from .product import Check
+
+
+class Move:
+
+    import sys
+    Check.check_product_installation(sys.argv[1])
+
--- learnPython/checkinstallation/product.py
+++ learnPython/checkinstallation/product.py
@@ -1,19 +1,36 @@
-def check_product_installation():
+class Check:
 
-    """  Checks Fiorano Product installation
+    def check_product_installation(self):
+
+        """  Checks Fiorano Product installation
     
         This module checks whether the product Fiorano installation is existed in the Specified directory or not.
         If exists, it moves the existing installation to new folder with SOMETHING appended to newly created folder
         """
     import sys
+    import os
     try:
-        directory, installation_directory = "fiorano",  sys.argv[1]
-        print(directory, installation_directory)
+        directory, installation_directory = "Fiorano",  sys.argv[1]
+        os.chdir(installation_directory)
+        directory_list = os.listdir(installation_directory)
+        for directory_name in directory_list:
+            if directory_name == "Fiorano":
+                os.rename('Fiorano', 'FioranoOld')
+                pass
         pass
     except ValueError:
         print('Passed String is not of String type')
+        pass
+    except OSError:
+        print('Passed installation_directory  is not valid for the Platform')
+        pass
+    pass
+pass
 
-check_product_installation()
+
+def get_build_number():
+    pass
+
 
 
 
