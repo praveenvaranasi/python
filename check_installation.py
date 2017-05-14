0f2085a30edc9f478a67da9aa5c41db2d8223c38
Praveen <praveenb.varanasi@outlook.com>
2017-05-14 19:00:18 +0530

    Completed writing the code that checks for existing Installation in Specified directory (Only for linux family as of now)

--- check_installation.py
+++ check_installation.py
@@ -6,6 +6,11 @@ property_file = os.path.join(installation_directory, directory, str(version), "b
 
 
 def get_build_number(self):
+
+    """ Gets the build number
+    
+        This function searches the build number in the Installation home of Fiorano and returns the build number"""
+
     content_string = []
     try:
         file = open(property_file)
@@ -13,25 +18,36 @@ def get_build_number(self):
             # print(line, end="")
             content_string.append(line)
         file.close()
-        print(content_string[-7:-2])
+        # print(content_string[-7:-2])
         build_number = ''.join(content_string[-7:-2])
         return directory+build_number
         pass
     except FileNotFoundError:
         print('The file build.properties did not exist in the specified path: ', property_file)
-    pass
+        pass
+    except ValueError:
+        print('hi')
+        pass
 pass
 
 
 def check_product_installation(self):
+
+    """Renames the Fiorano Home in the passed directory argument if exists
+    
+        This Function takes the directory as an argument and searches for Fiorano_home there. If Fiorano_Home exists, it
+        calls get_build_number function to get build number to append to the Fiorano. If not, does nothing by throwing 
+        an error"""
     try:
         os.chdir(installation_directory)
         directory_list = os.listdir(installation_directory)
-        for directory_name in directory_list:
-            if directory_name == "Fiorano":
-                new_directory = get_build_number(installation_directory)
-                os.rename('Fiorano', new_directory)
-                pass
+        if directory_list.count('Fiorano') == 0:
+            print('Fiorano Installation did not exist in the specified directory')
+        else:
+            print('Found Fiorano directory')
+            new_directory = get_build_number(installation_directory)
+            os.rename('Fiorano', new_directory)
+            print('Renamed the existing Installation directory to ', new_directory)
             pass
         pass
     except ValueError:
@@ -40,7 +56,10 @@ def check_product_installation(self):
     except OSError:
         print('Passed installation_directory  is not valid for the Platform')
         pass
-    pass
+    except TypeError:
+        print('The Fiorano is just an directory with no Installation Files. So renaming it to FioranoOld')
+        os.rename('Fiorano', 'FioranoOld')
+        pass
 pass
 
 check_product_installation(sys.argv[1])
--- learnPython/strings.py
+++ learnPython/strings.py
@@ -1,21 +1,27 @@
-#Strings
+# Strings
 sampleString = 'python'
-print("The length of the sampleString is" ,len(sampleString))
+print("The length of the sampleString is", len(sampleString))
 print(sampleString[0:])
 print(sampleString[0:4])
 print(sampleString[:3])
 print(sampleString[:])
 print(sampleString[:100])
-print(sampleString[100:]) #prints nothing
+print(sampleString[100:])  # prints nothing
 
-#List
-sampleList=[1,2,3,4,5]
+# List
+sampleList = [1, 2, 3, 4, 5]
 print("Then length of the sampleList is", len(sampleList))
-sampleList[0]=2
+sampleList[0] = 2
 print(sampleList[:])
-print(sampleList[0]+sampleList[3]);
+print(sampleList[0] + sampleList[3]);
 
-#Normal-String
-#Execute the very following line to encounter the error when running.
-#print('C:\Users\ndmin\Desktop')
+# Normal-String
+# Execute the very following line to encounter the error when running.
+# print('C:\Users\ndmin\Desktop')
 print(r'C:\Users\admin\nDesktop')
+
+if sampleList.count(1) == 0:
+    print('there!!')
+
+
+
