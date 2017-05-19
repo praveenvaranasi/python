dfb6ddc0d6a261c5d553a3145abe8968a2bfe5a1
Praveen <praveenb.varanasi@outlook.com>
2017-05-19 18:38:42 +0530

    Added module for different types of argument passing methods

---
+++ learnPython/positional_keyword_arguments.py
@@ -0,0 +1,13 @@
+def pos_key_args(message, default_arg="default", keyword_arg="keyword_arg"):
+    print(message)
+    print(default_arg)
+    print(keyword_arg)
+    pass
+
+
+# pos_key_args('greet!!!')
+
+if __name__ == "__main__":
+    pos_key_args('Hi, Hello Dude!!')
+    pos_key_args()
+    pass
