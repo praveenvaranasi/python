51e092b5774d14d84725688c408930c433e236ef
Srividya Tangirala <praveenb.varanasi@outlook.com>
2017-06-17 09:04:05 +0530

    Added a module to enhance the renaming mechanism of build number

--- learnPython/test.py
+++ learnPython/test.py
@@ -1,6 +1,21 @@
-praveen = True;
-if praveen:
-    print ("success");
+def check_build_number():
 
-while True:
+    build = input("Enter the build number:\n")
+    build_numbers = [10561, 10562]
+    print(len(build_numbers))
+    for i in build_numbers:
+        print(i)
+        if i == build:
+            resultant_build_number = int(build + "1")
+            print(resultant_build_number)
+        pass
+    # for i in range(0, 10):
+    #     resultant_build_number = resultant_build_number + i
+    #     print(resultant_build_number, end=",")
+    # pass
+
+if __name__ == "__main__":
+
+    check = check_build_number  # Renaming mechanism of the function
+    check()
     pass
