59c050d57236009d3ae2114ff7c1bb5d9ee09594
praveen <praveenb.varanasi@outlook.com>
2017-07-30 15:08:14 +0530

    Completed writing the logic to add resources in the file

--- add_resources.py
+++ add_resources.py
@@ -1,44 +1,24 @@
 import xml.etree.ElementTree as ET
-# from xml.etree.ElementTree import Element
 
 
 def microservice_resources():
+    """ The module adds resources"""
     try:
-        xml_tree = ET.ElementTree()
-        # xml_tree = ElementTree.parse("/root/Desktop/OldServiceDescriptor.xml")
-        # root = xml_tree.getroot()
-        # print(root.tag)
-        # deployment_element = root.findall('deployment')
-        # deployment_tag = deployment_element[0].tag
-        # resources_element = deployment_element[0].findall('resources')
-        # print(resources_element[0].tag)
-        # elem = ElementTree.Element(resources_element[0].tag)
-        # sub_elem = ElementTree.SubElement(elem, 'hello')
-        # last_elem = ElementTree.dump(elem)
-        # print(sub_elem)
-        # ElementTree.dump(xml_tree)
-        # task = ElementTree.Element(resources_element[0].tag)
-        # trying = ElementTree.SubElement(task, 'hello')
-        # xml_tree.ElementTree.write("/root/Desktop/Outputfinal")
-
-        tree = ET.parse('/root/Desktop/OldServiceDescriptor.xml')
-        # tree.parse("/root/Desktop/OldServiceDescriptor.xml")
+        tree = ET.parse('D:\Fiorano\AdditionOfJarsToEPT\ServiceDescriptor.xml')
         root = tree.getroot()
         deployment_elem = (root.findall('deployment'))[0]
         resources_elem = (deployment_elem.findall('resources'))[0]
-        print(resources_elem)
-        resources_elem.set('updated', 'yes')
         elem = ET.Element(resources_elem)
         sub_elem = ET.SubElement(elem, 'resource')
-        # tree.write('/root/Desktop/Outputshow.xml')
-        # root.insert([4][0], sub_elem)
-        root.insert(resources_elem, sub_elem)
-        tree.write('/root/Desktop/Outputshow.xml')
+        sub_elem.set('resource', 'hi')
+        resources_elem.insert(0, sub_elem)
+        tree.write('D:\Fiorano\AdditionOfJarsToEPT\Output.xml')
 
+    except AttributeError:
+        print('The attribute you are looking for is not appearing here')
+    except FileNotFoundError:
+        print('The file is not present in the specified location')
 
-    except TypeError:
-        # print('The attribute you are looking for is not appearing here')
-        pass
 if __name__ == "__main__":
     microservice_resources()
     pass
