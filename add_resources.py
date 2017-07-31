08c7fe4b3763934d44ea9701b5b738e64bf828ee
Praveen <praveenb.varanasi@outlook.com>
2017-07-31 19:52:42 +0530

    Added attribute and values

--- add_resources.py
+++ add_resources.py
@@ -4,15 +4,15 @@ import xml.etree.ElementTree as ET
 def microservice_resources():
     """ The module adds resources"""
     try:
-        tree = ET.parse('D:\Fiorano\AdditionOfJarsToEPT\ServiceDescriptor.xml')
+        tree = ET.parse('/root/Desktop/installed/ServiceDescriptor.xml')
         root = tree.getroot()
         deployment_elem = (root.findall('deployment'))[0]
         resources_elem = (deployment_elem.findall('resources'))[0]
         elem = ET.Element(resources_elem)
         sub_elem = ET.SubElement(elem, 'resource')
-        sub_elem.set('resource', 'hi')
+        sub_elem.set('name', 'postgresql-9.1-901.jdbc3.jar')
         resources_elem.insert(0, sub_elem)
-        tree.write('D:\Fiorano\AdditionOfJarsToEPT\Output.xml')
+        tree.write('/root/Desktop/installed/Fiorano/11/runtimedata/repository/components/jdbc/4.0/ServiceDescriptor.xml')
 
     except AttributeError:
         print('The attribute you are looking for is not appearing here')
